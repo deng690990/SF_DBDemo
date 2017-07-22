15a6c9cd730234c29d9777b8ec9365cdeb3f6265
deng690990 <164100551@qq.com>
2017-07-22 10:51:52 +0800

    Initial Commit

---
+++ 数据存储方式/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  数据存储方式
+//
+//  Created by 首牛 on 2017/7/22.
+//  Copyright © 2017年 ShouNew.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ 数据存储方式/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  数据存储方式
+//
+//  Created by 首牛 on 2017/7/22.
+//  Copyright © 2017年 ShouNew.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
